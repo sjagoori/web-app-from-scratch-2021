9db477f6e3c5eaf5e1114efdf83eae84d555ffda
sjagoori <sjagoori@users.noreply.github.com>
2021-02-04 06:32:14 +0100

    + cache getter setter functions

---
+++ spa/static/js/modules/cache.js
@@ -0,0 +1,17 @@
+/**
+ * Function sets data in localstorage
+ * @param {String} key key
+ * @param {Object} data data
+ */
+export function setCache(key, data){
+  localStorage.setItem(key, JSON.stringify(data))
+  console.log('setcache')
+}
+
+/**
+ * Function returns value from localstorage
+ * @param {String} key 
+ */
+export function getCache(key){
+  return JSON.parse(localStorage.getItem(key));
+}
