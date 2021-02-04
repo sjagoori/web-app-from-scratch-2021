e4f5570ccd688d5113d5b9ee1151c8191db6e118
sjagoori <sjagoori@users.noreply.github.com>
2021-02-05 00:36:12 +0100

    + added 2 more coins for perfect grid

--- spa/static/js/app.js
+++ spa/static/js/app.js
@@ -2,7 +2,7 @@ import { getData } from './modules/api.js'
 import { render } from './modules/render.js'
 
 async function main() {
-  let topList = await getData('https://shacors.herokuapp.com/https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?limit=10&CMC_PRO_API_KEY=a50e1e84-9b13-4d34-86e5-8b386c49c31c')
+  let topList = await getData('https://shacors.herokuapp.com/https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?limit=12&CMC_PRO_API_KEY=a50e1e84-9b13-4d34-86e5-8b386c49c31c')
   let infoList = await getData('https://shacors.herokuapp.com/https://pro-api.coinmarketcap.com/v1/cryptocurrency/info?id=' + topList.data.data.map(item => item.id).toString() + '&CMC_PRO_API_KEY=a50e1e84-9b13-4d34-86e5-8b386c49c31c')
 
   let merged = Object.values(infoList.data.data).map(key => {
