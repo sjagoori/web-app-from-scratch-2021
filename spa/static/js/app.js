59d582c28a56006a6e269149ccfc4fdf1ad2a962
sjagoori <sjagoori@users.noreply.github.com>
2021-02-04 08:50:28 +0100

    + data merge

--- spa/static/js/app.js
+++ spa/static/js/app.js
@@ -1,6 +1,16 @@
 import { printLine, setTitle } from './modules/test.js'
+import { getData } from './modules/api.js'
+import { render } from './modules/render.js'
 
-const titles = ['lol', 'lmao', 'testing']
-printLine('Hello');
+// menu.addEventListener('click', e => {
+//   console.log(e)
+// })
 
-titles.map(key => setTitle(key))
+async function main(){
+  let topList = await getData('https://shacors.herokuapp.com/https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?limit=10&CMC_PRO_API_KEY=a50e1e84-9b13-4d34-86e5-8b386c49c31c')
+  let infoList = await getData('https://shacors.herokuapp.com/https://pro-api.coinmarketcap.com/v1/cryptocurrency/info?id='+ topList.data.data.map(item => item.id).toString() +'&CMC_PRO_API_KEY=a50e1e84-9b13-4d34-86e5-8b386c49c31c')
+
+  render(infoList.data.data)
+}
+
+main()
