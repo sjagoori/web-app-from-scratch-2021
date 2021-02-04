a9d6a8aeb0032e8e502ebcf46d1b6a0caf3c9d4c
sjagoori <sjagoori@users.noreply.github.com>
2021-02-04 13:45:35 +0100

    + added quote/price data to pair

--- spa/static/js/app.js
+++ spa/static/js/app.js
@@ -1,14 +1,17 @@
-import { printLine, setTitle } from './modules/test.js'
 import { getData } from './modules/api.js'
 import { render } from './modules/render.js'
 
-// menu.addEventListener('click', e => {
-//   console.log(e)
-// })
-
-async function main(){
+async function main() {
   let topList = await getData('https://shacors.herokuapp.com/https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?limit=10&CMC_PRO_API_KEY=a50e1e84-9b13-4d34-86e5-8b386c49c31c')
-  let infoList = await getData('https://shacors.herokuapp.com/https://pro-api.coinmarketcap.com/v1/cryptocurrency/info?id='+ topList.data.data.map(item => item.id).toString() +'&CMC_PRO_API_KEY=a50e1e84-9b13-4d34-86e5-8b386c49c31c')
+  let infoList = await getData('https://shacors.herokuapp.com/https://pro-api.coinmarketcap.com/v1/cryptocurrency/info?id=' + topList.data.data.map(item => item.id).toString() + '&CMC_PRO_API_KEY=a50e1e84-9b13-4d34-86e5-8b386c49c31c')
+
+  let merged = Object.values(infoList.data.data).map(key => {
+    return key.quote = Object.values(topList.data.data)
+      .map(item => item.name == key.name ? item.quote.USD : false)
+      .filter(item => typeof item === 'object')
+  });
+
+  ({ ...infoList.data.data, merged }, delete infoList.data.data.merged)
 
   render(infoList.data.data)
 }
