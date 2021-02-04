7f056f3b42be377fd1844cc23cd9e6e1f407f982
sjagoori <sjagoori@users.noreply.github.com>
2021-02-04 06:49:53 +0100

    ~ return getCache value; fixed undefined on initial run

--- spa/static/js/modules/api.js
+++ spa/static/js/modules/api.js
@@ -3,12 +3,12 @@ import { getCache, setCache } from './cache.js'
 /**
  * Function makes an API calls and caches them if they're not cached before
  * @param {String} url API endpoint
+ * @param {Object} header Header params
  */
 export async function getData(url, header) {
   return getCache(url) ? getCache(url) : await fetch(url, header ? header : null)
     .then(response => response.json())
     .then(data => ({ url: url, data: data }))
-    .then(data => setCache(url, data))
-    .then(getCache(url))
+    .then(data => (setCache(url, data), getCache(url)))
     .catch(error => console.log(error))
 }
