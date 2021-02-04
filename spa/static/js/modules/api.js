f190234bd1202bfb001ccb90f6613c1c726ec14f
sjagoori <sjagoori@users.noreply.github.com>
2021-02-04 06:32:59 +0100

    + data fetch function + [WIP] auto-caching

---
+++ spa/static/js/modules/api.js
@@ -0,0 +1,14 @@
+import { getCache, setCache } from './cache.js'
+
+/**
+ * Function makes an API calls and caches them if they're not cached before
+ * @param {String} url API endpoint
+ */
+export async function getData(url, header) {
+  return getCache(url) ? getCache(url) : await fetch(url, header ? header : null)
+    .then(response => response.json())
+    .then(data => ({ url: url, data: data }))
+    .then(data => setCache(url, data))
+    .then(getCache(url))
+    .catch(error => console.log(error))
+}
