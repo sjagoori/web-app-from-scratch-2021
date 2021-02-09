063cba5854cd63c662f48dba68a305b3b6a5b283
sjagoori <sjagoori@users.noreply.github.com>
2021-02-09 16:44:39 +0100

    + comments

--- spa/static/js/app.js
+++ spa/static/js/app.js
@@ -5,12 +5,14 @@ async function main() {
   let topList = await getData('https://shacors.herokuapp.com/https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?limit=12&CMC_PRO_API_KEY=a50e1e84-9b13-4d34-86e5-8b386c49c31c')
   let infoList = await getData('https://shacors.herokuapp.com/https://pro-api.coinmarketcap.com/v1/cryptocurrency/info?id=' + topList.data.data.map(item => item.id).toString() + '&CMC_PRO_API_KEY=a50e1e84-9b13-4d34-86e5-8b386c49c31c')
 
+  //  adds quote array from toplist to infolist
   let merged = Object.values(infoList.data.data).map(key => {
     return key.quote = Object.values(topList.data.data)
       .map(item => item.name == key.name ? item.quote.USD : false)
       .filter(item => typeof item === 'object')
   });
 
+  // merges newly made object to existing object, removes remaining object 
   ({ ...infoList.data.data, merged }, delete infoList.data.data.merged)
 
   render(infoList.data.data)
