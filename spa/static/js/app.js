f85b93d06fc123547b66e13a1cb3ec5058fc74f0
sjagoori <sjagoori@users.noreply.github.com>
2021-02-09 17:09:06 +0100

    + condition for loading state

--- spa/static/js/app.js
+++ spa/static/js/app.js
@@ -1,5 +1,6 @@
 import { getData } from './modules/api.js'
 import { render } from './modules/render.js'
+const loader = document.getElementsByTagName('main')[0]
 
 async function main() {
   let topList = await getData('https://shacors.herokuapp.com/https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?limit=12&CMC_PRO_API_KEY=a50e1e84-9b13-4d34-86e5-8b386c49c31c')
@@ -15,7 +16,7 @@ async function main() {
   // merges newly made object to existing object, removes remaining object 
   ({ ...infoList.data.data, merged }, delete infoList.data.data.merged)
 
-  render(infoList.data.data)
+  render(infoList.data.data) ? loader.style.display = 'none' : null
 }
 
 main()
