ecd071b799355c394417ed021724336456d50792
sjagoori <sjagoori@users.noreply.github.com>
2021-02-19 17:04:20 +0100

    + docs

--- static/js/modules/colors.js
+++ static/js/modules/colors.js
@@ -1,3 +1,6 @@
+/**
+ * Funtion returns a set of colors for the tags in detailpages
+ */
 export function getColors(){
   return ['#95D7AE', '#B5AA9D', '#EBB3A9', '#E09891', '#CAB1BD', '#E7CFCD', '#CFD8D7', '#55C1FF']
 }
